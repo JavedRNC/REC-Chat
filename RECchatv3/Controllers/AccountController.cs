d8901c2ff57ebb7006f6e8bbbc17c2b4981f4f19
Javed <javedansarirnc@gmail.com>
2019-11-20 11:19:20 +0530

    myfirstGit

---
+++ RECchatv3/Controllers/AccountController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMatrix.WebData;
+
+namespace RECchatv3.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login(string returnUrl)
+        {
+            if(returnUrl!=null)
+            {
+                ViewBag.isReturnUrl = true; ;
+            }
+            ViewBag.Msg = "";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+                
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string Username, string Password )
+        {               
+            bool isAuthenticated = WebSecurity.Login(Username, Password);
+            if(isAuthenticated)
+            {
+                return RedirectToAction("Dashboard", "Account");
+            }
+            else
+            {
+                ViewBag.Msg = "Wrong Credentials, Please Contact Admin !";
+            }
+            return View();
+        }
+        [Authorize]
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Logout()
+        {
+            WebSecurity.Logout();
+            TempData["Logoutmsg"] = "You have been successfully logged out";
+            
+            return RedirectToAction("Login");
+        }
+
+        [Authorize]
+        public ActionResult ITCR()
+        {   
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Chat()
+        {
+            return View();
+        }
+    }
+}
---
+++ RECchatv3/Global.asax.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using WebMatrix.WebData;
+using System.Web.Security;
+using System.Data.SqlClient;
+
+namespace RECchatv3
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            InitialiseAuthenticationProcess();
+        }
+
+        private void InitialiseAuthenticationProcess()
+        {
+            if (!WebSecurity.Initialized)
+            {
+                WebSecurity.InitializeDatabaseConnection("dbx", "UsersTable", "Username", "Password", true);
+                //WebSecurity.CreateUserAndAccount("admin", "admin");
+                //Roles.CreateRole("Admin");
+                //Roles.CreateRole("SuperModerator");
+                //Roles.CreateRole("Moderator");
+                //Roles.AddUserToRole("admin", "admin");
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
